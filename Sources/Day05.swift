b8de92b8536f7e58e5bafab6d093d289b3fb0a77
Domenico Flauto <18469530+domenicoflauto@users.noreply.github.com>
2024-12-06 00:02:04 +0000

    Day 5 - removes commented lines

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -16,47 +16,6 @@ struct Day05: AdventDay {
   }
 
   func part1() -> Any {
-//    Goal: Identify which updates are already in the right order
-    
-//    Page number is correct if there are rules that put each other page after/before it.
-//    For example:
-//    47|53
-//    97|13
-//    97|61
-//    97|47
-//    75|29
-//    61|13
-//    75|53
-//    29|13
-//    97|29
-//    53|29
-//    61|53
-//    97|53
-//    61|29
-//    47|13
-//    75|47
-//    97|75
-//    47|61
-//    75|61
-//    47|29
-//    75|13
-//    53|13
-//
-//    75,47,61,53,29
-//    97,61,53,29,13
-//    75,29,13
-//    75,97,47,61,53
-//    61,13,29
-//    97,13,75,29,47
-    
-//    In the above example, the first update (75,47,61,53,29) is in the right order:
-//
-//    75 is correctly first because there are rules that put each other page after it: 75|47, 75|61, 75|53, and 75|29.
-//    47 is correctly second because 75 must be before it (75|47) and every other page must be after it according to 47|61, 47|53, and 47|29.
-//    61 is correctly in the middle because 75 and 47 are before it (75|61 and 47|61) and 53 and 29 are after it (61|53 and 61|29).
-//    53 is correctly fourth because it is before page number 29 (53|29).
-//    29 is the only page left and so is correctly last.
-    
     var count = 0
     
     let pageOrderingRules = entities[0].map {
